f6a024e3b2b6057bc635465c883ae4a691d032bb
wixon1337 <51904857+wixon1337@users.noreply.github.com>
2022-11-21 21:10:40 +0100

    Card design. Card pairing logic implemented. Counter created.

--- lib/components/board.dart
+++ lib/components/board.dart
@@ -12,6 +12,9 @@ class Board extends StatefulWidget {
 
 class _BoardState extends State<Board> {
   final List<MyCard> _cards = [];
+  MyCard? revealedCard;
+  bool _tapLock = false;
+  int _counter = 0;
 
   @override
   void initState() {
@@ -21,19 +24,85 @@ class _BoardState extends State<Board> {
 
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 2,
-      children: _cards.map((card) => card.image).toList(),
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  const Text('Current tries: '),
+                  Text('$_counter'),
+                ],
+              ),
+              Column(
+                children: const [
+                  Text('Best:'),
+                  Text('9'),
+                ],
+              ),
+              ElevatedButton(onPressed: () {}, child: const Text('Restart')),
+            ],
+          ),
+          Expanded(
+            child: GridView.extent(
+              maxCrossAxisExtent: 300.0,
+              children: _cards
+                  .map(
+                    (card) => Card(
+                      child: InkWell(
+                        onTap: _tapLock || card.isRevealed ? null : () => _cardOnTap(card),
+                        child: card.isRevealed ? card.image : null,
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   void _init() {
     var numberOfUniqueCards = widget.numberOfCards ~/ 2;
-    var cardIndexes = List.generate(numberOfUniqueCards, (index) => index + 1).toList();
+    var cardIndexes = List.generate(numberOfUniqueCards, (index) => index).toList();
     for (var index in cardIndexes) {
       _cards.add(MyCard(index));
       _cards.add(MyCard(index));
     }
     _cards.shuffle();
+  }
+
+  void _cardOnTap(MyCard card) {
+    if (revealedCard == null) {
+      setState(() {
+        card.isRevealed = true;
+        revealedCard = card;
+      });
+    } else {
+      _counter++;
+      if (revealedCard!.index == card.index) {
+        setState(() {
+          card.isRevealed = true;
+          revealedCard = null;
+        });
+      } else {
+        setState(() {
+          card.isRevealed = true;
+          _tapLock = true;
+        });
+        Future.delayed(const Duration(seconds: 1), () {
+          setState(() {
+            card.isRevealed = false;
+            revealedCard!.isRevealed = false;
+            revealedCard = null;
+            _tapLock = false;
+          });
+        });
+      }
+    }
   }
 }
